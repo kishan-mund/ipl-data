852ac109b111b0cf263240a378db21bbd9121737
Sachin M K <sachinmk27@gmail.com>
2019-03-11 17:34:48 +0530

    Remove solution

--- src/ipl.js
+++ src/ipl.js
@@ -1,10 +1,4 @@
-export const getNoOfMatchesPlayed = matches => {
-  return matches.reduce((noOfMatchesPlayed, match) => {
-    noOfMatchesPlayed[match.season] = noOfMatchesPlayed[match.season] || 0;
-    noOfMatchesPlayed[match.season] += 1;
-    return noOfMatchesPlayed;
-  }, {});
-};
+export const getNoOfMatchesPlayed = () => {};
 export const getNoOfMatchesWonPerTeamPerYear = () => {};
 export const getExtraRunsPerTeamForYear = () => {};
 export const getEconomicalBowlersForYear = () => {};
